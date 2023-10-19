e1c98b257f40c8f84a6f6ff5eb84158887a385ce
조세은 <>
2023-10-19 13:58:23 +0900

    refresh code

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue';
+import Login from './components/Login.vue';
+import Sighin from './components/Signin.vue';
+import Todolist from './components/Todolist.vue';
+</script>
+
+<template>
+  <div>
+    기본 앱 .뷰
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <Todolist />
+  <Login />
+  <Sighin />
+</template>
+
+<style scoped></style>
---
+++ src/components/Signin.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+<template>회원가입 구현</template>
+
+<style scoped></style>
